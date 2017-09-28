ed14be00b5b4b4a74741a0a7d2876f4619f27183
diana <diana@hml-spro4.lexicon.edu.local>
2017-09-28 13:44:20 +0200

    added Member, MemberDao, MemberRest

---
+++ restproject/src/main/java/org/diana/MemberRest.java
@@ -0,0 +1,52 @@
+package org.diana;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.tools.examples.model.Member;
+
+@Path("/members")
+@RequestScoped
+public class MemberRest {
+
+	@Inject
+	MemberDao memberdao;
+
+	@POST
+	@Path("/creatememner")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public void addMember(Member member) {
+		memberdao.createMember(member);
+	}
+
+	@POST
+	@Path("/updatePerson")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public void refreshPerson(Member member) {
+		memberdao.updateMember(member);
+	}
+
+	@POST
+	@Path("/deletePerson")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMember(Member member) {
+		memberdao.removeMember(member);
+	}
+
+	@GET
+	@Path("/members")
+	public List<Member> mem(Member member) {
+		return memberdao.getAll(member);
+	}
+}
